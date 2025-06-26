358cf068aa69140f7d9b0f90fd29289e5f0eeeea
kwuebben <126663878+kwuebben@users.noreply.github.com>
2025-06-26 12:41:10 +0200

    Update functions.R
    Undo changes regarding minus strand

--- functions.R
+++ functions.R
@@ -6,6 +6,9 @@ frame_counts = function(count, dir = '+', utr_length = 50){
   if(length(count) > (2*utr_length)){
     # check if dividable by 3
     if( (length(count)-(2*utr_length))%%3 == 0){
+      if(dir == '-'){
+        count <- rev(count)
+      }
       # cut utr
       count <- count[(utr_length+1):(length(count)-utr_length)]
       # count frames
@@ -26,6 +29,9 @@ frame_counts = function(count, dir = '+', utr_length = 50){
 norm_counts_start_stop = function(count, dir = '+', utr_length = 50, cds_length = 100, normalize = TRUE) {
   # check length
   if(length(count) > (cds_length + (2 * utr_length))){
+    if(dir == '-'){
+        count <- rev(count)
+      }
     if(normalize){
       # get mean 
       c_mean <- mean(count[(utr_length+1):(length(count)-utr_length)], na.rm = T)
